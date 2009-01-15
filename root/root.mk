cb2b79979d3b3aa935ff77b2094383102f629c75
Thomas Monjalon <tmonjalo@grp-tmonjalon.(none)>
2009-01-15 11:33:14 +0100

    root.mk: work in progress

--- root/root.mk
+++ root/root.mk
@@ -1,14 +1,17 @@
+ROOT_SKEL_DIR = $(ROOT_DIR)/skel
 ROOT_BUILD_DIR = $(BUILD_DIR)/$(ROOT_DIR)
 ROOT_BUILD_LIB_DIR = $(ROOT_BUILD_DIR)/lib
 ROOT_BUILD_BIN_DIRS += $(ROOT_BUILD_DIR)/bin $(ROOT_BUILD_DIR)/sbin
 
-.PHONY: root root_update_libs
+.PHONY: root root_lib_init root_lib root_bin_init root_bin root_skel root_dev_init root_dev root_clean
 clean: root_clean
 
-root: busybox packages root_libs
+# scheduling rule
+root: busybox packages root_lib root_bin root_skel root_dev
 
-root_libs:
+root_lib_init:
 	@ echo '=== LIBRARIES ==='
+root_lib: root_lib_init $(MKLIBS) $(SSTRIP)
 	mkdir -p $(ROOT_BUILD_LIB_DIR)
 	$(SET_CROSS_PATH) $(MKLIBS) \
 		$(if $(CROSS_PREFIX), --target $(CROSS_PREFIX)) \
@@ -16,6 +19,22 @@ root_libs:
 		--dest-dir $(ROOT_BUILD_LIB_DIR) \
 		$(foreach DIR, $(ROOT_BUILD_BIN_DIRS), $(DIR)/*)
 	$(SSTRIP) $(ROOT_BUILD_LIB_DIR)/*
+
+root_bin_init:
+	@ echo '=== BINARIES ==='
+root_bin: root_bin_init $(SSTRIP)
+	$(foreach DIR, $(ROOT_BUILD_BIN_DIRS), $(SSTRIP) $(DIR)/* ;)
+
+root_skel:
+	@ echo '=== SKELETON ==='
+	cp -a $(ROOT_SKEL_DIR)/* $(ROOT_BUILD_DIR)
+
+root_dev_init:
+	@ echo '=== DEVICES ==='
+root_dev: root_dev_init $(MAKEDEVS)
+	fakeroot sh -c "chown -R 0:0 $(ROOT_BUILD_DIR) ; ls -l $(ROOT_BUILD_DIR)/dev ; \
+		$(MAKEDEVS) -d $(ROOT_DIR)/device_table.txt $(ROOT_BUILD_DIR) ; \
+		ls -l $(ROOT_BUILD_DIR)/dev"
 
 root_clean:
 	- rm -rf $(ROOT_BUILD_DIR)
