9eb52aa3a3c0e830e9749ad0d9089ff6d44143d1
Thomas Monjalon <tmonjalo@grp-tmonjalon.(none)>
2009-01-16 09:56:49 +0100

    simplify image.mk thanks to the use of LINUX_INITRAMFS

--- image/image.mk
+++ image/image.mk
@@ -1,26 +1,23 @@
 IMAGE_BUILD_DIR = $(BUILD_DIR)/$(IMAGE_DIR)
 FAKEROOT_SCRIPT = $(IMAGE_BUILD_DIR)/fakeroot.sh
-ROOT_CPIO = $(IMAGE_BUILD_DIR)/root.cpio
-ROOT_CPIO_GZ = $(IMAGE_BUILD_DIR)/root.gz
+ROOT_CPIO = $(IMAGE_BUILD_DIR)/$(if $(LINUX_INITRAMFS),root.cpio,root.gz)
 
 .PHONY: image image_init image_clean
 clean: image_clean
 
-# scheduling rule
-image_linux_initramfs: linux root $(ROOT_CPIO)
-image_root_gz: root $(ROOT_CPIO_GZ)
+image: root $(ROOT_CPIO)
 
 image_init:
 	@ echo '=== IMAGE ==='
 	mkdir -p $(IMAGE_BUILD_DIR)
 
 $(ROOT_CPIO): root_dev image_init
-	echo 'cd $(ROOT_BUILD_DIR) && find | cpio --quiet --create --format=newc > $(abspath $@)' >> $(FAKEROOT_SCRIPT)
-	fakeroot sh $(FAKEROOT_SCRIPT)
-
-$(ROOT_CPIO_GZ): root_dev image_init
-	echo 'cd $(ROOT_BUILD_DIR) && find | cpio --quiet --create --format=newc | gzip --best > $(abspath $@)' >> $(FAKEROOT_SCRIPT)
-	fakeroot sh $(FAKEROOT_SCRIPT)
+	echo 'cd $(ROOT_BUILD_DIR) && find \
+		| cpio --quiet --create --format=newc \
+		$(if $(LINUX_INITRAMFS),,| gzip --best) > $(abspath $@)' \
+		>> $(FAKEROOT_SCRIPT)
+	fakeroot sh -x $(FAKEROOT_SCRIPT)
+	@ echo 'image = $@'
 
 image_clean:
 	- rm -rf $(IMAGE_BUILD_DIR)
