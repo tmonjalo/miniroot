0760e29c52306dc8e94a6f92907123c153330455
Thomas Monjalon <tmonjalo@grp-tmonjalon.(none)>
2009-01-20 19:25:22 +0100

    use ROOT_BUILD_LIB_DIR in install rules and in mklibs (as input)

--- packages/zlib/zlib.mk
+++ packages/zlib/zlib.mk
@@ -13,7 +13,7 @@ ZLIB_SRC_DIR = $(shell $(TOOLS_DIR)/get_src_dir.sh '$(ZLIB_DIR)' '$(ZLIB_SRC)')
 ZLIB_VERSION = $(shell sed -n 's,.*VERSION.*"\(.*\)".*,\1,p' $(ZLIB_SRC_DIR)/zlib.h 2>/dev/null)
 ZLIB_BUILD_DIR = $(ZLIB_SRC_DIR)
 ZLIB_BUILD_BIN = $(ZLIB_BUILD_DIR)/libz.so.$(ZLIB_VERSION)
-ZLIB_INSTALL_BIN = $(ROOT_BUILD_DIR)/lib/$(notdir $(ZLIB_BUILD_BIN))
+ZLIB_INSTALL_BIN = $(ROOT_BUILD_LIB_DIR)/$(notdir $(ZLIB_BUILD_BIN))
 
 .PHONY: zlib zlib_init zlib_configure zlib_clean
 $(eval $(call PKG_INCLUDE_RULE, $(PKG_ZLIB), zlib))
@@ -37,6 +37,7 @@ $(ZLIB_BUILD_BIN): zlib_init
 $(ZLIB_INSTALL_BIN): $(ZLIB_BUILD_BIN)
 	install -D $(ZLIB_BUILD_BIN) $@
 	$(CROSS_STRIP) $@
+	find $(ZLIB_BUILD_DIR) -type l -name "lib*" -exec cp -P '{}' $(ROOT_BUILD_LIB_DIR) \;
 
 zlib_clean:
 	- $(MAKE) -C $(ZLIB_BUILD_DIR) clean
