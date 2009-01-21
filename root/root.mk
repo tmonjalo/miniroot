e8a71bd9e4fb7ff7ef84aeee887dc2a52c61f9cd
Thomas Monjalon <tmonjalo@grp-tmonjalon.(none)>
2009-01-21 14:58:50 +0100

    do not use previously installed libs to construct libs

--- root/root.mk
+++ root/root.mk
@@ -14,7 +14,7 @@ root_lib: root_lib_init $(MKLIBS) $(SSTRIP)
 	mkdir -p $(ROOT_BUILD_LIB_DIR)
 	$(SET_CROSS_PATH) $(MKLIBS) \
 		$(if $(CROSS_PREFIX), --target $(CROSS_PREFIX)) \
-		-D $(foreach DIR, $(CROSS_LIB_DIRS), -L $(DIR)) -L $(ROOT_BUILD_LIB_DIR) \
+		-D $(foreach DIR, $(CROSS_LIB_DIRS), -L $(DIR)) \
 		--dest-dir $(ROOT_BUILD_LIB_DIR) \
 		$(foreach DIR, $(ROOT_BUILD_BIN_DIRS), $(shell find $(DIR) -type f))
 	find $(ROOT_BUILD_LIB_DIR) -type f | xargs -r $(SSTRIP) 2>/dev/null || true
