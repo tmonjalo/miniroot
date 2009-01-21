5c504781c9d024b56435ddaa9b915ed9d769a3aa
Thomas Monjalon <tmonjalo@grp-tmonjalon.(none)>
2009-01-21 15:00:05 +0100

    do not install zlib but use it via CROSS_LIB_DIRS

--- packages/zlib/zlib.mk
+++ packages/zlib/zlib.mk
@@ -13,12 +13,11 @@ ZLIB_SRC_DIR = $(shell $(TOOLS_DIR)/get_src_dir.sh '$(ZLIB_DIR)' '$(ZLIB_SRC)')
 ZLIB_VERSION = $(shell sed -n 's,.*VERSION.*"\(.*\)".*,\1,p' $(ZLIB_SRC_DIR)/zlib.h 2>/dev/null)
 ZLIB_BUILD_DIR = $(ZLIB_SRC_DIR)
 ZLIB_BUILD_BIN = $(ZLIB_BUILD_DIR)/libz.so.$(ZLIB_VERSION)
-ZLIB_INSTALL_BIN = $(ROOT_BUILD_LIB_DIR)/$(notdir $(ZLIB_BUILD_BIN))
 
 .PHONY: zlib zlib_init zlib_configure zlib_clean
 $(eval $(call PKG_INCLUDE_RULE, $(PKG_ZLIB), zlib))
 
-zlib: $(ZLIB_INSTALL_BIN)
+zlib: $(ZLIB_BUILD_BIN)
 
 zlib_init:
 	@ echo '=== ZLIB ==='
@@ -29,16 +28,13 @@ zlib_init:
 
 zlib_configure:
 	( cd $(ZLIB_BUILD_DIR) && \
-	$(SET_CROSS_PATH) $(SET_CROSS_CC) CFLAGS='$(CROSS_CFLAGS) -fPIC' ./configure --shared )
+		$(SET_CROSS_PATH) $(SET_CROSS_CC) CFLAGS='$(CROSS_CFLAGS) -fPIC' ./configure --shared \
+	)
 
 $(ZLIB_BUILD_BIN): zlib_init
 	$(SET_CROSS_PATH) $(MAKE) -C $(ZLIB_BUILD_DIR) $(notdir $@)
 
-$(ZLIB_INSTALL_BIN): $(ZLIB_BUILD_BIN)
-	install -D $(ZLIB_BUILD_BIN) $@
-	$(CROSS_STRIP) $@
-	find $(ZLIB_BUILD_DIR) -type l -name "lib*" -exec cp -P '{}' $(ROOT_BUILD_LIB_DIR) \;
-
 zlib_clean:
 	- $(MAKE) -C $(ZLIB_BUILD_DIR) clean
-	- rm -f $(ZLIB_INSTALL_BIN)
+
+CROSS_LIB_DIRS += $(ZLIB_BUILD_DIR)
