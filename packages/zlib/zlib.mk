9edf8d4ce89ca0a9de2fcf11de81e0c58e514b0b
Thomas Monjalon <thomas@monjalon.net>
2009-04-07 14:36:03 +0200

    fix zlib package for static lib

--- packages/zlib/zlib.mk
+++ packages/zlib/zlib.mk
@@ -14,7 +14,7 @@ endif
 ZLIB_SRC_DIR = $(shell $(TOOLS_DIR)/get_src_dir.sh '$(ZLIB_DIR)' '$(ZLIB_SRC)')
 ZLIB_VERSION = $(shell sed -n 's,.*VERSION.*"\(.*\)".*,\1,p' $(ZLIB_SRC_DIR)/zlib.h 2>/dev/null)
 ZLIB_BUILD_DIR = $(if $(ZLIB_BUILD_INSIDE), $(ZLIB_SRC_DIR), $(BUILD_DIR)/$(notdir $(ZLIB_DIR)))
-ZLIB_BUILD_BIN = $(ZLIB_BUILD_DIR)/libz.so.$(ZLIB_VERSION)
+ZLIB_BUILD_BIN = $(ZLIB_BUILD_DIR)/libz.$(if $(TARGET_STATIC),a,so.$(ZLIB_VERSION))
 
 .PHONY: zlib zlib_init zlib_configure zlib_clean
 $(eval $(call PKG_INCLUDE_RULE, $(PKG_ZLIB), zlib))
@@ -28,11 +28,11 @@ zlib_init:
 zlib_configure:
 	( cd $(ZLIB_BUILD_DIR) && \
 		$(SET_CROSS_PATH) $(SET_CROSS_CC) CFLAGS='$(TARGET_CFLAGS) -fPIC' $(SET_LDFLAGS) \
-		./configure --shared \
+		$(if $(TARGET_STATIC), ./configure, ./configure --shared) \
 	)
 
 $(ZLIB_BUILD_BIN): zlib_init
-	@ if fgrep -q 'LIBS=libz.a' $(ZLIB_SRC_DIR)/Makefile ; then \
+	@ if ! fgrep -q 'LIBS=$(notdir $(ZLIB_BUILD_BIN))' $(ZLIB_SRC_DIR)/Makefile ; then \
 		$(MAKE) zlib_configure ; \
 	fi
 	$(SET_CROSS_PATH) $(MAKE) -C $(ZLIB_BUILD_DIR) $(notdir $(ZLIB_BUILD_BIN))
