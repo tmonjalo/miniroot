884dc7fa2714cb7b2be8d42edf00ae5e378f8dfa
Hugues Hiegel <hugues@hiegel.fr>
2009-04-10 18:20:39 +0200

    native-gcc makefile improvments

--- packages/native-gcc/native-gcc.mk
+++ packages/native-gcc/native-gcc.mk
@@ -1,21 +1,33 @@
-
+##
+## config.mk available options ##
+##
 # A target prefix for the target rootfs.
 # Could be, f.e., /usr/local, or /opt/, or whatever..
-TARGET_PREFIX ?= 
-
-NATIVEGCC_PREFIX		= $(if $(CROSS_PATH),$(CROSS_PATH)/)$(patsubst %-,%,$(CROSS_PREFIX))
-# The folders where we could find anything needed to compile on target.
+NATIVEGCC_TARGET_PREFIX	?= 
+# A prefix where we would find the native gcc binaries, libs and includes
+NATIVEGCC_PREFIX		?= $(CROSS_PATH)/$(CROSS_PREFIX:-=)
+# The binaries, lib and includes folders to copy to rootfs
 NATIVEGCC_DIRECTORIES	?= bin lib sys-include
-NATIVEGCC_INSTALLDIR	= $(ROOT_BUILD_DIR)/$(TARGET_PREFIX)
+# Verbosity of native-gcc installation
+SET_NATIVEGCC_VERBOSE	= $(if $(VERBOSE),NATIVEGCC_VERBOSE="yes")
+##
+## end of config.mk tunable variables ##
+##
+
+NATIVEGCC_INSTALLDIR = $(ROOT_BUILD_DIR)/$(NATIVEGCC_TARGET_PREFIX)
 
 .PHONY: native-gcc native-gcc_install
 $(eval $(call PKG_INCLUDE_RULE, $(PKG_NATIVEGCC), native-gcc))
 
+# An empty rule to make it more readable from packages.mk
 native-gcc: native-gcc_install
+
+# The real stuff
 native-gcc_install: 
-	@echo "== NATIVE GCC INSTALL =="
-	$(foreach DIR,$(NATIVEGCC_DIRECTORIES),\
-		echo "$(DIR)..." ; \
+	@echo "== NATIVE GCC =="
+	@$(foreach DIR,$(NATIVEGCC_DIRECTORIES),\
+		$(if $(NATIVEGCC_VERBOSE),echo,:) "Installing $(DIR)..." ; \
 		mkdir -p $(NATIVEGCC_INSTALLDIR)/$(DIR) ;\
 		tar -cC $(NATIVEGCC_PREFIX)/$(DIR) . | tar -xC $(NATIVEGCC_INSTALLDIR)/$(DIR) ;\
 	)
+
