d0e2ab7703d76629e5193edf35b8c6da15778313
Thomas Monjalon <thomas@monjalon.net>
2009-07-01 13:32:26 +0200

    update access to buildroot repository

--- tools/external_tools.mk
+++ tools/external_tools.mk
@@ -2,13 +2,12 @@ PATCH_KERNEL = $(TOOLS_DIR)/patch-kernel.sh
 MKLIBS = $(TOOLS_DIR)/mklibs.py
 tools : $(PATCH_KERNEL) $(MKLIBS)
 
+BUILDROOT_URL = http://git.buildroot.net/buildroot/plain/
 BUILDROOT_PATHS = \
 	toolchain/patch-kernel.sh \
 	toolchain/sstrip/sstrip.c \
 	toolchain/mklibs/mklibs.py \
 	target/makedevs/makedevs.c
-BUILDROOT_URL_PREFIX = http://sources.busybox.net/index.py/trunk/buildroot/
-BUILDROOT_URL_SUFFIX = ?view=co
 
 define EXTERNAL_TOOLS_DOWNLOAD
 $1 :
@@ -19,7 +18,7 @@ $1 :
 endef
 $(foreach PATH, $(BUILDROOT_PATHS), $(eval $(call EXTERNAL_TOOLS_DOWNLOAD, \
 	$(TOOLS_DIR)/$(notdir $(PATH)), \
-	$(BUILDROOT_URL_PREFIX)$(PATH)$(BUILDROOT_URL_SUFFIX), \
+	$(BUILDROOT_URL)$(PATH), \
 	$(suffix $(PATH)) \
 )))
 
