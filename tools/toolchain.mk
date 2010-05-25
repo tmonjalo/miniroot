3391f0b82a8097c3f988e4739b585203c4dc6bc1
Thomas Monjalon <thomas@monjalon.net>
2010-05-25 22:37:29 +0200

    toolchain: update config with oldconfig
    Old versions don't allow to do
    	yes '' | ct-ng oldconfig
    An upgrade of crosstool-ng is needed.

--- tools/toolchain.mk
+++ tools/toolchain.mk
@@ -25,13 +25,19 @@ TOOLCHAIN_BUILD_DIR := $(BUILD_DIR)/$(TOOLCHAIN_PREFIX:-=)
 TOOLCHAIN_BUILD_CONFIG := $(TOOLCHAIN_BUILD_DIR)/.config
 TOOLCHAIN_DEFAULT_CONFIG = $(CROSSTOOL-NG_SRC_DIR)/samples/$(TOOLCHAIN_PREFIX:-=)/crosstool.config
 
+TOOLCHAIN_MAKE = $(abspath $(CROSSTOOL-NG)) -C $(TOOLCHAIN_BUILD_DIR)
+TOOLCHAIN_MAKE_OLDCONFIG = yes '' | $(TOOLCHAIN_MAKE) oldconfig >/dev/null
+
 TOOLCHAIN_CONFIG_SET = sed -i 's,^\($(strip $1)\).*,\1="$(strip $2)",' $(TOOLCHAIN_BUILD_CONFIG)
 TOOLCHAIN_CONFIG_ENABLE = sed -i '/^$(strip $1).*/d' $(TOOLCHAIN_BUILD_CONFIG) && echo $(strip $1)=y >> $(TOOLCHAIN_BUILD_CONFIG)
 TOOLCHAIN_CONFIG_DISABLE = sed -i 's,^\($(strip $1)\).*,\# \1 is not set,' $(TOOLCHAIN_BUILD_CONFIG)
 
-.PHONY : toolchain toolchain_init toolchain_config toolchain_clean
+.PHONY : toolchain toolchain_init toolchain_clean
 
 toolchain : $(TOOLCHAIN_DEP)
+
+$(TOOLCHAIN_CC) :
+	@ $(MAKE) --no-print-directory toolchain_build
 
 toolchain_init :
 	@ printf '\n=== TOOLCHAIN ===\n'
@@ -44,20 +50,16 @@ $(TOOLCHAIN_BUILD_CONFIG) :
 	else \
 		cp $(TOOLCHAIN_DEFAULT_CONFIG) $@ ; \
 	fi
-
-toolchain_config : toolchain_init $(TOOLCHAIN_BUILD_CONFIG)
 	$(call TOOLCHAIN_CONFIG_SET,     CT_LOCAL_TARBALLS_DIR     , $(abspath $(DL_DIR)))
 	$(call TOOLCHAIN_CONFIG_ENABLE,  CT_SAVE_TARBALLS          )
 	$(call TOOLCHAIN_CONFIG_SET,     CT_WORK_DIR               , $${CT_TOP_DIR}/tmp)
 	$(call TOOLCHAIN_CONFIG_SET,     CT_PREFIX_DIR             , $${CT_TOP_DIR}/toolchain)
 	$(if $(TOOLCHAIN_UCLIBC_CONFIG), \
 	$(call TOOLCHAIN_CONFIG_SET,     CT_LIBC_UCLIBC_CONFIG_FILE, $(abspath $(TOOLCHAIN_UCLIBC_CONFIG))))
+	$(TOOLCHAIN_MAKE_OLDCONFIG)
 
-toolchain_% : $(CROSSTOOL-NG) toolchain_config
-	cd $(TOOLCHAIN_BUILD_DIR) && $(abspath $(CROSSTOOL-NG)) $*
-
-$(TOOLCHAIN_CC) :
-	@ $(MAKE) --no-print-directory toolchain_build
+toolchain_% : $(CROSSTOOL-NG) toolchain_init $(TOOLCHAIN_BUILD_CONFIG)
+	$(TOOLCHAIN_MAKE) $*
 
 toolchain_clean :
 	- chmod -R +w $(TOOLCHAIN_BUILD_DIR)
