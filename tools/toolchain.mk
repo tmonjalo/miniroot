1610ea91af34f4712f6de10898e92729828a2b47
Thomas Monjalon <thomas@monjalon.net>
2010-10-12 17:14:07 +0200

    toolchain: build in HOST_BUILD_DIR and split build/install
    The defaults are:
    	build/host/toolchain/PREFIX
    	build/host/toolchain/PREFIX.config
    	build/host/toolchain/PREFIX.tmp
    
    The built toolchain is the first component to use the new organization
    of the build directory.
    
    A name can be used instead of the computed PREFIX.
    
    Only one toolchain is built. A dedicated toolchain for Linux only won't be
    built. It could be fixed later.

--- tools/toolchain.mk
+++ tools/toolchain.mk
@@ -3,15 +3,15 @@ HOST_CC ?= gcc
 #TOOLCHAIN_PATH =
 TOOLCHAIN_PREFIX ?= $(if $(TARGET_ARCH), $(TARGET_ARCH)-unknown-linux-gnu-)
 TARGET_CC ?= ccache $(notdir $(TOOLCHAIN_CC))
-TARGET_CXX ?= $(patsubst %g,%,$(TARGET_CC:gcc=))g++
+TARGET_CXX ?= $(strip $(patsubst %g,%,$(TARGET_CC:gcc=)))g++
 
 TOOLCHAIN_PATH_PREFIX = $(if $(TOOLCHAIN_PATH), $(strip $(TOOLCHAIN_PATH))/bin/$(strip $(TOOLCHAIN_PREFIX)))
-TOOLCHAIN_CC = $(TOOLCHAIN_PATH_PREFIX)gcc
+TOOLCHAIN_CC = $(strip $(TOOLCHAIN_PATH_PREFIX))gcc
 
 ifneq '$(TARGET_ARCH)' ''        # if ARCH defined
 ifeq '$(TOOLCHAIN_PATH)' ''      # and PATH undefined
 TOOLCHAIN_DEP = $(TOOLCHAIN_CC)  # then build the toolchain
-TOOLCHAIN_PATH = $(TOOLCHAIN_BUILD_DIR)/toolchain
+TOOLCHAIN_PATH = $(TOOLCHAIN_INSTALL_DIR)
 tools : toolchain
 clean : toolchain_clean crosstool-ng_clean
 endif
@@ -21,16 +21,18 @@ TARGET_LIB_DIRS += $(if $(TOOLCHAIN_PATH), $(strip $(TOOLCHAIN_PATH))/lib)
 
 include $(TOOLS_DIR)/toolchain/crosstool-ng.mk
 
-TOOLCHAIN_BUILD_DIR := $(BUILD_DIR)/$(TOOLCHAIN_PREFIX:-=)
-TOOLCHAIN_BUILD_CONFIG := $(TOOLCHAIN_BUILD_DIR)/.config
-TOOLCHAIN_DEFAULT_CONFIG = $(CROSSTOOL-NG_SRC_DIR)/samples/$(TOOLCHAIN_PREFIX:-=)/crosstool.config
+TOOLCHAIN_BUILD_DIR := $(strip $(HOST_BUILD_DIR))/toolchain/$(strip $(TARGET_NAME)).tmp
+TOOLCHAIN_CONFIG_DIR := $(strip $(HOST_BUILD_DIR))/toolchain/$(strip $(TARGET_NAME)).config
+TOOLCHAIN_CONFIG := $(strip $(TOOLCHAIN_CONFIG_DIR))/.config
+TOOLCHAIN_DEFAULT_CONFIG = $(strip $(CROSSTOOL-NG_SRC_DIR))/samples/$(strip $(TOOLCHAIN_PREFIX:-=))/crosstool.config
+TOOLCHAIN_INSTALL_DIR := $(strip $(HOST_BUILD_DIR))/toolchain/$(strip $(TARGET_NAME))
 
-TOOLCHAIN_MAKE = $(abspath $(CROSSTOOL-NG)) -C $(TOOLCHAIN_BUILD_DIR)
+TOOLCHAIN_MAKE = $(abspath $(CROSSTOOL-NG)) -C $(TOOLCHAIN_CONFIG_DIR)
 TOOLCHAIN_MAKE_OLDCONFIG = yes '' | $(TOOLCHAIN_MAKE) oldconfig >/dev/null
 
-TOOLCHAIN_CONFIG_SET = sed -i 's,^\($(strip $1)\).*,\1="$(strip $2)",' $(TOOLCHAIN_BUILD_CONFIG)
-TOOLCHAIN_CONFIG_ENABLE = sed -i '/^$(strip $1).*/d' $(TOOLCHAIN_BUILD_CONFIG) && echo $(strip $1)=y >> $(TOOLCHAIN_BUILD_CONFIG)
-TOOLCHAIN_CONFIG_DISABLE = sed -i 's,^\($(strip $1)\).*,\# \1 is not set,' $(TOOLCHAIN_BUILD_CONFIG)
+TOOLCHAIN_CONFIG_SET = sed -i 's,^\($(strip $1)\).*,\1="$(strip $2)",' $(TOOLCHAIN_CONFIG)
+TOOLCHAIN_CONFIG_ENABLE = sed -i '/^$(strip $1).*/d' $(TOOLCHAIN_CONFIG) && echo $(strip $1)=y >> $(TOOLCHAIN_CONFIG)
+TOOLCHAIN_CONFIG_DISABLE = sed -i 's,^\($(strip $1)\).*,\# \1 is not set,' $(TOOLCHAIN_CONFIG)
 
 .PHONY : toolchain toolchain_init toolchain_clean
 
@@ -42,7 +44,7 @@ $(TOOLCHAIN_CC) :
 toolchain_init :
 	@ printf '\n=== TOOLCHAIN ===\n'
 
-$(TOOLCHAIN_BUILD_CONFIG) :
+$(TOOLCHAIN_CONFIG) :
 	mkdir -p $(@D)
 	@ echo 'copy config to $@'
 	@ if [ -f '$(strip $(TOOLCHAIN_CONFIG))' ] ; then \
@@ -52,15 +54,19 @@ $(TOOLCHAIN_BUILD_CONFIG) :
 	fi
 	$(call TOOLCHAIN_CONFIG_SET,     CT_LOCAL_TARBALLS_DIR     , $(abspath $(DL_DIR)))
 	$(call TOOLCHAIN_CONFIG_ENABLE,  CT_SAVE_TARBALLS          )
-	$(call TOOLCHAIN_CONFIG_SET,     CT_WORK_DIR               , $${CT_TOP_DIR}/tmp)
-	$(call TOOLCHAIN_CONFIG_SET,     CT_PREFIX_DIR             , $${CT_TOP_DIR}/toolchain)
+	$(call TOOLCHAIN_CONFIG_SET,     CT_WORK_DIR               , $(abspath $(TOOLCHAIN_BUILD_DIR)))
+	$(call TOOLCHAIN_CONFIG_SET,     CT_PREFIX_DIR             , $(abspath $(TOOLCHAIN_INSTALL_DIR)))
 	$(if $(TOOLCHAIN_UCLIBC_CONFIG), \
 	$(call TOOLCHAIN_CONFIG_SET,     CT_LIBC_UCLIBC_CONFIG_FILE, $(abspath $(TOOLCHAIN_UCLIBC_CONFIG))))
 	$(TOOLCHAIN_MAKE_OLDCONFIG)
 
-toolchain_% : $(CROSSTOOL-NG) toolchain_init $(TOOLCHAIN_BUILD_CONFIG)
+toolchain_% : $(CROSSTOOL-NG) toolchain_init $(TOOLCHAIN_CONFIG)
 	$(TOOLCHAIN_MAKE) $*
 
 toolchain_clean :
-	- chmod -R +w $(TOOLCHAIN_BUILD_DIR)
+	- rm -rf $(TOOLCHAIN_CONFIG_DIR)
 	- rm -rf $(TOOLCHAIN_BUILD_DIR)
+
+toolchain_distclean : toolchain_clean
+	- chmod -R +w $(TOOLCHAIN_INSTALL_DIR)
+	- rm -rf $(TOOLCHAIN_INSTALL_DIR)
